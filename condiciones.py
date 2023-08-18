38f971a59794e2bdda2504a39d5a1d02250562c6
osvaldoc10 <osvaldocausado10@outloo.es>
2023-08-18 10:17:20 -0500

    NTF3 se inicia condicionales
    clear

---
+++ condiciones.py
@@ -0,0 +1,9 @@
+edadUsuario=int(input("digita tu edad: "))
+
+if edadUsuario>=18:
+    print("Bienvenido a la disco")
+else:
+    print("Pa la casa por ser menor")
+    print("Oeeee pa la casaaa...")
+
+ 
---
+++ temperatura.py
@@ -0,0 +1,7 @@
+temperatura=int(input("Digite la temperatura"))
+gradoAlcohol=int(input("Ingrese los grados de alcohol"))
+
+if temperatura>50 or gradoAlcohol>5:
+    print("PONGALE CUIDADO")
+else:
+    print("NO PASA NARA MI FAFA")
